2c93f877917648ebf7b1383a9822b4e6ec007319
Иван Нигаев <Nihaiev@it.group>
2022-07-12 21:00:36 +0300

    example 2

--- js/callback.js
+++ js/callback.js
@@ -27,3 +27,48 @@
 // fnA('qwerty', fnB);
 
 //! Example2
+
+// const doMath = function (a, b, callback) {
+//     const result = callback(a, b);
+//     console.log(result);
+    
+// }
+
+// const add = function (x, y) { 
+//     return x + y;
+// }
+
+// const sub = function (x, y) { //
+//     return x - y;
+// }
+
+// doMath(2, 3, add);
+// doMath(10, 8, sub);
+
+
+/**
+ * Обявили функцію add кинули її doMath(2, 3, add); аргументом 
+ * записався в парметр callback, викликали callback, a, b докинулись як x, y
+ * воно їх порахувало return x + y; і повернуло результат на місце виклику result
+ * у змінну result буквально записався виклик add після чого ми його залогували console.log(result);
+ */
+
+
+// ЛІтерал функції
+
+// const doMath = function (a, b, callback) {
+//     const result = callback(a, b);
+//     console.log(result);
+    
+// }
+
+// doMath(2, 3, function (x, y) {
+//     return x + y;
+// }); // літерал фукції, ми просто взяли оголошення функції і передали як третій аргумент
+// doMath(10, 8, function (x, y) {
+//     return x - y;
+// }); //  це називається інлайн функція
+
+
+
+
