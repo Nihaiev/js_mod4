1a8ad6bdf48acdb1cce8b54c4e65700862e90f48
Иван Нигаев <Nihaiev@it.group>
2022-07-16 12:31:38 +0300

    Update foreach.js

--- js/foreach.js
+++ js/foreach.js
@@ -16,3 +16,29 @@ numbers.forEach(function (number, index, array) {
  * Тобто перший аргумент це функція, тобто що зробити 
  * Друге це обєкт в контексті якого цю функцію необхідно викликати 
  */
+
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Change code below this line
+
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] > value) {
+  //     filteredNumbers.push(numbers[i]);
+  //   }
+  // }
+  numbers.forEach(function (number) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  // Change code above this line
+  return filteredNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3))
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
