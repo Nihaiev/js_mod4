8a9369615468c3aa35c5a07cd3d13b1573eebe38
Ivan <nigaev.ivan@gmail.com>
2022-09-04 16:59:49 +0300

    arrow syntax

---
+++ js/synrax.js
@@ -0,0 +1,52 @@
+// const add=function(a,b){
+//return a + b;
+//};
+
+/**
+ * Синатксис стрілкової функції =>
+ *  - з апарметрами
+ *  - з одним параметром
+ *  - без параметрів
+ */
+
+// переписуємо функцію вище на =>
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(add(2, 3));
+
+//якщо параметр 1
+
+// const logMessage = message => {
+//   console.log(message);
+// };
+
+// console.log(logMessage('Hello'));
+
+//якщо параметрів взагалі нема () - ставлять пусті пузаті скобки
+// const greet = () => {
+//   console.log('Hello');
+// };
+
+// greet();
+
+/**
+ * явне повернення (explicid)
+ * це коли є тіло функції і вньому явно вказано повернення return
+ */
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(add(2, 3));
+
+/**
+ * Неявне (implicit)
+ */
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 3));
