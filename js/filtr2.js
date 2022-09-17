ebf16baad71b4f8dd19ce587c1f9472b9e72a822
Ivan <nigaev.ivan@gmail.com>
2022-09-17 13:10:15 +0300

    Update filtr2.js

--- js/filtr2.js
+++ js/filtr2.js
@@ -25,3 +25,5 @@ const players = [
   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
 ];
+
+//39
