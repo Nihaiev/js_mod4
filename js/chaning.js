576622b7f3dff2fe60336bc64f96b2da8fe96f92
Ivan <nigaev.ivan@gmail.com>
2022-09-25 15:36:27 +0300

    Update chaning.js

--- js/chaning.js
+++ js/chaning.js
@@ -21,6 +21,7 @@ console.log(sortd);
  * Ланцюг попередніг трьох
  * - це працює завдяки тому що ці методи повератють новий масив,
  * виходить ось такий ланцюг
+ *
  */
 const sorted = numbers
   .filter(num => num > 2)
@@ -28,3 +29,44 @@ const sorted = numbers
   .sort((a, b) => a - b); // на результаті map викликається sort
 
 console.log(sorted);
+
+/**
+ * Сортуємо тих хто онлайн по рангу
+ *  - з початку фільруємо
+ *  - потім сортуємо
+ * - якщо нам треба зробити заперечення ставимо просто ! (не)
+ */
+
+const players = [
+  { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
+  { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
+  { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
+  { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
+];
+
+const onlineAndSorted = players
+  .filter(player => player.isOnline)
+  .sort((playerA, playerB) => playerA.rank - playerB.rank);
+
+console.table(onlineAndSorted);
+
+/**
+ * Chaining в методах обєкта як jquery
+ * jquery - це бібліотека для роботи з інтерфейсом
+ */
+
+const element = {
+  class: '',
+  hovered: false,
+  changeClass(cls) {
+    this.class = cls;
+    return this;
+  },
+  toggleHovered() {
+    this.hovered = !this.hovered;
+    return this;
+  },
+};
+
+element.toggleHovered().changeClass('open').toggleHovered();
+console.log(element);
